e1ea7f63e8033f73132ad5fcf73519c91ade4713
SergioK12 <sergioabd460@gmail.com>
2023-03-13 02:39:43 +0100

    Agregado el path inicial para ser visto en el puerto

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+const pathPublic = path.resolve(__dirname, 'public')
+app.use(express.static(pathPublic))
 app.listen( '3000', 
     (err)=> {
         if(err){
