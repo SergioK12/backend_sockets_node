eb85cb8daa16a46548c4f7c04dbbe3477e93401e
SergioK12 <sergioabd460@gmail.com>
2023-03-15 03:13:34 +0100

    Agregados los scripts de socket y la config basica

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express')
 const path = require('path')
 require('dotenv').config()
+//Express App
 const app = express()
+
+//Node Server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Mensajes de sockets
+io.on('connection', client => {
+        console.log("cliente conectado")
+   
+    client.on('disconnect', () => {
+
+        console.log("cliente desconectado")
+
+    });
+  });
 
 const pathPublic = path.resolve(__dirname, 'public')
 app.use(express.static(pathPublic))
-app.listen( process.env.PORT, 
+server.listen( process.env.PORT, 
     (err)=> {
         if(err){
             throw new Error(err)
         }
         console.log('Servidor corriendo en', process.env.PORT)
-        console.log("test 02")
     }
 )
