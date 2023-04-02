5ad041f19fd4bd136a28e029c5f1e1dcfc7ae993
Sergiok12 <sergioabd460@gmail.com>
2023-04-01 23:18:06 -0600

    Emitir y replicar datos en consola

--- sockets/socket.js
+++ sockets/socket.js
@@ -3,14 +3,16 @@ io.on('connection', client => {
     console.log("cliente conectado")
 
     client.on('disconnect', () => {
-
         console.log("cliente desconectado")
-
     });
 
-    client.on('Mensaje', (carga) => {
-
+    client.on('mensaje', (carga) => {
+        console.log('Mensaje: ',carga)  
         io.emit('m2', { nombre: "Sergio" })
     });
+
+    client.on('emitir-mensaje', (payload) => {
+        io.emit('nuevo-mensaje', payload);
+    })
 
 });
